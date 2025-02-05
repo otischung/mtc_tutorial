28a02dc86d9a1e237143cbe0a46664d3c0f7f81b
otischung <p76124605@gs.ncku.edu.tw>
2025-02-05 16:26:25 +0800

    [FIX] Add complete orientation quaternion

--- src/hello_moveit.cpp
+++ src/hello_moveit.cpp
@@ -20,10 +20,13 @@ int main(int argc, char* argv[]) {
     // Set a target Pose
     auto const target_pose = [] {
         geometry_msgs::msg::Pose msg;
-        msg.orientation.w = 1.0;
-        msg.position.x = 0.28;
-        msg.position.y = -0.2;
-        msg.position.z = 0.5;
+        msg.orientation.x = 0.34049764662684506;
+        msg.orientation.y = 0.2791014345764092;
+        msg.orientation.z = 0.5401637225908053;
+        msg.orientation.w = 0.7172077067738565;
+        msg.position.x = 0.24521369847453386;
+        msg.position.y = -0.6416689293249782;
+        msg.position.z = 1.3406012136001955;
         return msg;
     }();
     move_group_interface.setPoseTarget(target_pose);
