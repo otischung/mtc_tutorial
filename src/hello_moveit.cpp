81014bd9a6c6d79abd121b83194f96de470a4fc5
otischung <p76124605@gs.ncku.edu.tw>
2025-02-05 14:19:26 +0800

    [FIX] Modify move_group_interface.hpp to .h

--- src/hello_moveit.cpp
+++ src/hello_moveit.cpp
@@ -1,5 +1,5 @@
 #include <memory>
-#include <moveit/move_group_interface/move_group_interface.hpp>
+#include <moveit/move_group_interface/move_group_interface.h>
 #include <rclcpp/rclcpp.hpp>
 
 int main(int argc, char* argv[]) {
