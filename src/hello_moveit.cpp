601e23b804dc77143c11f522adefd27155e90b11
otischung <p76124605@gs.ncku.edu.tw>
2025-02-06 13:42:29 +0800

    [FEAT] Rotate using Eigen and TF2

--- src/hello_moveit.cpp
+++ src/hello_moveit.cpp
@@ -1,5 +1,21 @@
-#include <memory>
+#if __has_include(<tf2_geometry_msgs/tf2_geometry_msgs.hpp>)
+#include <moveit/move_group_interface/move_group_interface.hpp>
+#else
 #include <moveit/move_group_interface/move_group_interface.h>
+#endif
+#if __has_include(<tf2_geometry_msgs/tf2_geometry_msgs.hpp>)
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#else
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#endif
+#if __has_include(<tf2_eigen/tf2_eigen.hpp>)
+#include <tf2_eigen/tf2_eigen.hpp>
+#else
+#include <tf2_eigen/tf2_eigen.h>
+#endif
+
+#include <Eigen/Geometry>
+#include <memory>
 #include <rclcpp/rclcpp.hpp>
 
 int main(int argc, char* argv[]) {
@@ -20,14 +36,30 @@ int main(int argc, char* argv[]) {
     // Set a target Pose
     auto const target_pose = [] {
         geometry_msgs::msg::Pose msg;
-        msg.orientation.x = 0.06956080515042753;
-        msg.orientation.y = 0.07966487548583161;
-        msg.orientation.z = 0.7189456774179794;
-        msg.orientation.w = 0.686973008874915;
+        // Set orientation to identity (no rotation)
+        msg.orientation.x = 0.0;
+        msg.orientation.y = 0.0;
+        msg.orientation.z = 0.0;
+        msg.orientation.w = 1.0;
+        // Set position values
         msg.position.x = 0.32498640696298825;
         msg.position.y = -0.3024952440821608;
         msg.position.z = 1.001358523108688;
-        return msg;
+
+        // Convert geometry_msgs::msg::Pose to Eigen::Isometry3d using tf2::fromMsg
+        Eigen::Isometry3d eigen_transform;
+        tf2::fromMsg(msg, eigen_transform);
+
+        // Define a 90-degree (π/2 radians) rotation about the Z-axis
+        double angle_rad = M_PI / 2.0;  // 90 degrees in radians
+        Eigen::AngleAxisd rotation_vector(angle_rad, Eigen::Vector3d::UnitZ());
+
+        // Apply the rotation to the original transform
+        eigen_transform.rotate(rotation_vector);
+
+        // Convert the Eigen transform back to geometry_msgs::msg::Pose
+        geometry_msgs::msg::Pose rotated_pose = tf2::toMsg(eigen_transform);
+        return rotated_pose;
     }();
     move_group_interface.setPoseTarget(target_pose);
 
