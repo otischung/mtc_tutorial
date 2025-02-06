169400948964d465d1da878bc727cbd35df9d441
otischung <p76124605@gs.ncku.edu.tw>
2025-02-06 11:31:29 +0800

    [FIX] Set the position to the possible play piano pose

--- src/hello_moveit.cpp
+++ src/hello_moveit.cpp
@@ -20,13 +20,13 @@ int main(int argc, char* argv[]) {
     // Set a target Pose
     auto const target_pose = [] {
         geometry_msgs::msg::Pose msg;
-        msg.orientation.x = 0.34049764662684506;
-        msg.orientation.y = 0.2791014345764092;
-        msg.orientation.z = 0.5401637225908053;
-        msg.orientation.w = 0.7172077067738565;
-        msg.position.x = 0.24521369847453386;
-        msg.position.y = -0.6416689293249782;
-        msg.position.z = 1.3406012136001955;
+        msg.orientation.x = 0.06956080515042753;
+        msg.orientation.y = 0.07966487548583161;
+        msg.orientation.z = 0.7189456774179794;
+        msg.orientation.w = 0.686973008874915;
+        msg.position.x = 0.32498640696298825;
+        msg.position.y = -0.3024952440821608;
+        msg.position.z = 1.001358523108688;
         return msg;
     }();
     move_group_interface.setPoseTarget(target_pose);
